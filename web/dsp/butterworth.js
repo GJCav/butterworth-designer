e555e00c6b9c45a82d1764d2fb60dabb5be0b33f
JCav <micojcav@outlook.com>
2022-12-14 12:33:23 +0800

    finish code calculating N, w_c, pole degs, working on poles diagrams

---
+++ web/dsp/butterworth.js
@@ -0,0 +1,35 @@
+import * as mjs from "mathjs"
+
+export const pack_params = (w_p, w_s, G_0, A_p, A_s) => ({
+  w_p, w_s, G_0, A_p, A_s
+})
+
+export const calc_n = (params) => {
+  const { w_p, w_s, G_0, A_p, A_s } = params
+  // const r = w_p / w_s
+  const r = mjs.divide(w_p, w_s)
+  // const A = Math.pow(10, (A_p - A_s) / 10)
+  const A = mjs.pow(10, mjs.chain(A_p).subtract(A_s).divide(10).done())
+  // const B = Math.pow(10, -(G_0 + A_s) / 10)
+  const B = mjs.pow(10, mjs.chain(G_0).add(A_s).divide(10).multiply(-1).done())
+  const C = (A - B) / (1 - B)
+  const N = mjs.log(C) / mjs.log(r) / 2
+  return mjs.ceil(N)
+}
+
+export const calc_w_c = (data) => {
+  const { w_s, G_0, A_s, N } = data
+  const A = mjs.pow(10, mjs.chain(G_0).add(A_s).divide(-10).done())
+  const B = mjs.divide(A, 1 - A)
+  const w_c = mjs.pow(B, 1 / (2 * N)) * w_s
+  return w_c
+}
+
+export const calc_poles = (data) => {
+  const { N } = data
+  const poles = []
+  for (let k = 1; k <= N; k++) {
+    poles.push(((2 * k - 1) / (2 * N) + 1 / 2) * mjs.pi)
+  }
+  return poles
+}
--- web/pages/LPF.vue
+++ web/pages/LPF.vue
@@ -1,19 +1,19 @@
 <template>
   <v-container>
-    <h1>Design Params:</h1>
-    <v-row class="mt-2 align-baseline" dense>
+    <h2>目标参数:</h2>
+    <v-row class="mt-6 align-baseline" dense>
       <v-col cols="6">
         <v-row class="align-baseline">
           <v-col cols="3" style="text-align: right;">
-            <span class="text-h6">\(w_p = \)</span>
+            <span class="text-h6">\(f_p = \)</span>
           </v-col>
           <v-col cols="9">
-            <v-text-field v-model="w_p_i" label="通带频率" outlined dense></v-text-field>
+            <v-text-field v-model="f_p_i" label="通带频率" outlined dense></v-text-field>
           </v-col>
         </v-row>
       </v-col>
       <v-col cols="6">
-        <span class="text-h6">= {{ w_p_val }} Hz</span>
+        <span class="text-h6">= {{ f_p_val }} Hz</span>
       </v-col>
     </v-row>
 
@@ -21,15 +21,15 @@
       <v-col cols="6">
         <v-row class="align-baseline">
           <v-col cols="3" style="text-align: right;">
-            <span class="text-h6">\(w_s = \)</span>
+            <span class="text-h6">\(f_s = \)</span>
           </v-col>
           <v-col cols="9">
-            <v-text-field v-model="w_s_i" label="阻带频率" outlined dense></v-text-field>
+            <v-text-field v-model="f_s_i" label="阻带频率" outlined dense></v-text-field>
           </v-col>
         </v-row>
       </v-col>
       <v-col cols="6">
-        <span class="text-h6">= {{ w_s_val }} Hz</span>
+        <span class="text-h6">= {{ f_s_val }} Hz</span>
       </v-col>
     </v-row>
 
@@ -80,29 +80,52 @@
         <span class="text-h6">= {{ A_s_val }} dB</span>
       </v-col>
     </v-row>
+
+    <v-btn @click="solve_filter">Solve</v-btn>
   </v-container>
 </template>
 
 <script>
 import { extend_scale_unit } from "../dsp/parser"
+import * as BTW from "../dsp/butterworth"
 
 export default {
   name: "LPF",
 
   data: () => ({
-    w_p_i: "10M",
-    w_s_i: "20M",
-    G_0_i: "1",
+    f_p_i: "1M",
+    f_s_i: "1.5M",
+    G_0_i: "0",
     A_p_i: "1",
-    A_s_i: "100"
+    A_s_i: "15"
   }),
 
   computed: {
-    w_p_val() { return extend_scale_unit(this.w_p_i) },
-    w_s_val() { return extend_scale_unit(this.w_s_i) },
+    f_p_val() { return extend_scale_unit(this.f_p_i) },
+    f_s_val() { return extend_scale_unit(this.f_s_i) },
     G_0_val() { return extend_scale_unit(this.G_0_i) },
     A_p_val() { return extend_scale_unit(this.A_p_i) },
     A_s_val() { return extend_scale_unit(this.A_s_i) },
-  }
+  },
+
+  methods: {
+    solve_filter() {
+      const data = BTW.pack_params(
+        this.f_p_val * 2 * Math.PI,
+        this.f_s_val * 2 * Math.PI,
+        this.G_0_val,
+        this.A_p_val,
+        this.A_s_val
+      )
+      data.N = BTW.calc_n(data)
+      console.log(data.N)
+      data.w_c = BTW.calc_w_c(data)
+      console.log(data.w_c / Math.PI / 2)
+      data.poles = BTW.calc_poles(data)
+      for (const p of data.poles) {
+        console.log(p / Math.PI)
+      }
+    }
+  },
 }
 </script>
